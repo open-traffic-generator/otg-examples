f2c18f899524fd97a2e1f6e91c6a4f58f3ef4387
Alex Bortok <dev@bortok.com>
2022-12-02 16:51:52 -0800

    ixia-c-b2b with 3rd flow from scapy

--- clab/ixia-c-b2b/otg.py
+++ clab/ixia-c-b2b/otg.py
@@ -1,10 +1,17 @@
 import snappi
 import dpkt
+from scapy.all import *
 
 def port_metrics_ok(api, req, packets):
     res = api.get_metrics(req)
     print(res)
     if packets == sum([m.frames_tx for m in res.port_metrics]) and packets == sum([m.frames_rx for m in res.port_metrics]):
+        return True
+
+def flow_metrics_ok(api, req, packets):
+    res = api.get_metrics(req)
+    print(res)
+    if packets == sum([m.frames_tx for m in res.flow_metrics]) and packets == sum([m.frames_rx for m in res.flow_metrics]):
         return True
 
 def wait_for(func, timeout=15, interval=0.2):
@@ -24,7 +31,7 @@ def wait_for(func, timeout=15, interval=0.2):
     print("Timeout occurred !")
     return False
 
-api = snappi.api(location='https://clab-ixcb2b-ixia-c', verify=False)
+api = snappi.api(location='https://clab-ixcb2b-ixia-c:8443', verify=False)
 cfg = api.config()
 # this pushes object of type `snappi.Config` to controller
 api.set_config(cfg)
@@ -37,11 +44,12 @@ p1, p2 = cfg.ports.port(name="p1", location="eth1").port(name="p2", location="et
 
 # config has an attribute called `flows` which holds an iterator of type
 # `snappi.FlowIter`, where each item is of type `snappi.Flow` (f1, f2)
-f1, f2 = cfg.flows.flow(name="flow p1->p2").flow(name="flow p2->p1")
+f1, f2, f3 = cfg.flows.flow(name="flow p1->p2").flow(name="flow p2->p1").flow(name="scapy")
 
 # and assign source and destination ports for each
 f1.tx_rx.port.tx_name, f1.tx_rx.port.rx_name = p1.name, p2.name
 f2.tx_rx.port.tx_name, f2.tx_rx.port.rx_name = p2.name, p1.name
+f3.tx_rx.port.tx_name, f3.tx_rx.port.rx_name = p1.name, p2.name
 
 # configure packet size, rate and duration for both flows
 f1.size.fixed, f2.size.fixed = 128, 256
@@ -78,8 +86,18 @@ udp2.src_port.increment.count = 10
 udp1.dst_port.values = [4000, 4044, 4060, 4074]
 udp2.dst_port.values = [8000, 8044, 8060, 8074, 8082, 8084]
 
+# custom flow from scapy
+packet = IP(dst="10.11.12.13", src="10.11.12.14")/UDP()/DNS()
+f3.packet.ethernet().custom()
+
+eth3 = f3.packet[0]
+eth3.src.value, eth3.dst.value = "00:AA:00:00:04:00", "00:AA:00:00:00:AA"
+
+payload = f3.packet[1]
+payload.bytes = packet[0].payload.build().hex()
+
 # print resulting otg configuration
-# print(cfg)
+print(cfg)
 
 # push configuration to controller
 api.set_config(cfg)
@@ -91,13 +109,11 @@ api.set_transmit_state(ts)
 
 # create a port metrics request and filter based on port names
 req = api.metrics_request()
-req.port.port_names = [p.name for p in cfg.ports]
-# include only sent and received packet counts
-req.port.column_names = [req.port.FRAMES_TX, req.port.FRAMES_RX]
+req.flow.flow_names = [f.name for f in cfg.flows]
 
 # fetch port metrics
 res = api.get_metrics(req)
 
 # wait for port metrics to be as expected
 expected = sum([f.duration.fixed_packets.packets for f in cfg.flows])
-assert wait_for(lambda: port_metrics_ok(api, req, expected)), "Metrics validation failed!"
+assert wait_for(lambda: flow_metrics_ok(api, req, expected)), "Metrics validation failed!"
