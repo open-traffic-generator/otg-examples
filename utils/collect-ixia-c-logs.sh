1ce4fb31e40ee02c7fc913c82f746498bd7f707e
Himanshu Ashwini <himanshu.ashwini@keysight.com>
2023-05-16 12:52:19 -0400

    Adding script to collect ixia-c logs

---
+++ utils/collect-ixia-c-logs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CONTAINERS=$(docker ps --filter "name=ixia-c" --format "{{.Names}}")
+NOW=$(date +"%Y-%m-%d_%H%M%S")
+mkdir -p $NOW
+for CONTAINER in $CONTAINERS
+do
+	echo "Collecting logs for $CONTAINER"
+	LOGFILE="log-$CONTAINER-$NOW.log"
+	docker logs $CONTAINER > $NOW/$LOGFILE
+	echo "Done collecting logs for $CONTAINER"
+
+	if [[ "$CONTAINER" == *"ixia-c-ixhw-server"* ]]; then
+  		echo "Collecting internal logs for ixia-c-ixhw-server"
+		mkdir -p $NOW/hw
+		docker cp $CONTAINER:/tmp/rpf_logs/ $NOW/hw
+		docker cp $CONTAINER:/var/log/ $NOW/hw
+  		echo "Done collecting internal logs for ixia-c-ixhw-server"
+	fi
+	echo ""
+done
+
