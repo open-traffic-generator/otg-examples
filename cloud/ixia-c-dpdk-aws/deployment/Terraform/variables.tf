28ae218244464a4084c3a02695153a84e563e808
armdupre <a-joseph.dupre@keysight.com>
2024-03-15 16:26:41 -0400

    Updated.

--- cloud/ixia-c-dpdk-aws/deployment/Terraform/variables.tf
+++ cloud/ixia-c-dpdk-aws/deployment/Terraform/variables.tf
@@ -21,11 +21,13 @@ variable "ApiMaxRetries" {
 
 variable "AwsAccessCredentialsAccessKey" {
 	description = "Access key component of credentials used for programmatic calls to AWS."
+	sensitive = true
 	type = string
 }
 
 variable "AwsAccessCredentialsSecretKey" {
 	description = "Secret access key component of credentials used for programmatic calls to AWS."
+	sensitive = true
 	type = string
 }
 
