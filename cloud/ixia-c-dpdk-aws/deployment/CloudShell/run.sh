8ea3e216e9fa686f8d48dff4cd2f164ea40ba2e7
armdupre <a-joseph.dupre@keysight.com>
2024-04-10 02:28:46 -0400

    Initial Checkin.

---
+++ cloud/ixia-c-dpdk-aws/deployment/CloudShell/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+aws ec2 describe-instances \
+	--filters "Name=instance-state-name,Values=running" "Name=tag-value,Values=*agent1*" \
+	--query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" \
+	--output table
+Agent1Id=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag-value,Values=*agent1*" --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" --output json | jq .[0][0] --raw-output)
+aws ssm start-session \
+	--document-name 'AWS-StartInteractiveCommand' \
+	--parameters '{"command": ["sudo make run -C /home/ubuntu/keng-python/cloud/ixia-c-dpdk-aws/"]}' \
+	--target $Agent1Id
