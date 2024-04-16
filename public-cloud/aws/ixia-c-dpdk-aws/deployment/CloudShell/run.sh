e030cf1540e3a084b5bfacc2c7cc3553dd765e78
armdupre <a-joseph.dupre@keysight.com>
2024-04-16 11:26:33 -0400

    Updated.

--- public-cloud/aws/ixia-c-dpdk-aws/deployment/CloudShell/run.sh
+++ public-cloud/aws/ixia-c-dpdk-aws/deployment/CloudShell/run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-UserName="ubuntu"
-GitRepoName="keng-python"
-GitRepoBasePath="/home/$UserName/$GitRepoName/public-cloud/aws/ixia-c-dpdk-aws"
-GitRepoExecPath="$GitRepoBasePath/application"
 aws ec2 describe-instances \
 	--filters "Name=instance-state-name,Values=running" "Name=tag-value,Values=*agent1*" \
 	--query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" \
@@ -10,5 +6,5 @@ aws ec2 describe-instances \
 Agent1Id=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag-value,Values=*agent1*" --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" --output json | jq .[0][0] --raw-output)
 aws ssm start-session \
 	--document-name 'AWS-StartInteractiveCommand' \
-	--parameters '{"command": ["sudo make run -C $GitRepoExecPath"]}' \
+	--parameters '{"command": ["sudo make run -C /home/ubuntu/keng-python/public-cloud/aws/ixia-c-dpdk-aws/application"]}' \
 	--target $Agent1Id
