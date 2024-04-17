84f30290ee65803b6934dfec804713dc0cdd7b29
armdupre <a-joseph.dupre@keysight.com>
2024-04-17 01:56:33 -0400

    Updated.

--- public-cloud/aws/ixia-c-dpdk-aws/deployment/CloudShell/connect.sh
+++ public-cloud/aws/ixia-c-dpdk-aws/deployment/CloudShell/connect.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+while getopts i: flag
+do
+    case "${flag}" in
+        i) PublicIp=${OPTARG};;
+    esac
+done
+
 aws ec2 describe-instances \
-	--filters "Name=instance-state-name,Values=running" "Name=tag-value,Values=*agent1*" \
+	--filters "Name=instance-state-name,Values=running" "Name=network-interface.association.public-ip,Values=$PublicIp" \
 	--query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" \
 	--output table
-Agent1Id=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag-value,Values=*agent1*" --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" --output json | jq .[0][0] --raw-output)
+AgentId=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=network-interface.association.public-ip,Values=$PublicIp" --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" --output json | jq .[0][0] --raw-output)
 aws ssm start-session \
-	--target $Agent1Id
+	--target $AgentId
--- public-cloud/aws/ixia-c-dpdk-aws/deployment/CloudShell/run.sh
+++ public-cloud/aws/ixia-c-dpdk-aws/deployment/CloudShell/run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
+while getopts i: flag
+do
+    case "${flag}" in
+        i) PublicIp=${OPTARG};;
+    esac
+done
+
 aws ec2 describe-instances \
-	--filters "Name=instance-state-name,Values=running" "Name=tag-value,Values=*agent1*" \
+	--filters "Name=instance-state-name,Values=running" "Name=network-interface.association.public-ip,Values=$PublicIp" \
 	--query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" \
 	--output table
-Agent1Id=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag-value,Values=*agent1*" --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" --output json | jq .[0][0] --raw-output)
+AgentId=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=network-interface.association.public-ip,Values=$PublicIp" --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" --output json | jq .[0][0] --raw-output)
 aws ssm start-session \
 	--document-name 'AWS-StartInteractiveCommand' \
 	--parameters '{"command": ["sudo make run -C /home/ubuntu/keng-python/public-cloud/aws/ixia-c-dpdk-aws/application"]}' \
-	--target $Agent1Id
+	--target $AgentId
