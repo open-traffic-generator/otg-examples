47fe8a428f0226e70e8a5dbb1a3cac2adca275dd
armdupre <a-joseph.dupre@keysight.com>
2024-04-22 09:50:09 -0400

    Updated.

--- public-cloud/azure/ixia-c-dpdk-azure-mana/deployment/CloudShell/connect.sh
+++ public-cloud/azure/ixia-c-dpdk-azure-mana/deployment/CloudShell/connect.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-while getopts i: flag
+while getopts i:k: flag
 do
     case "${flag}" in
         i) PublicIp=${OPTARG};;
+		k) SshKey=${OPTARG};;
     esac
 done
 
-aws ec2 describe-instances \
-	--filters "Name=instance-state-name,Values=running" "Name=network-interface.association.public-ip,Values=$PublicIp" \
-	--query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" \
-	--output table
-AgentId=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=network-interface.association.public-ip,Values=$PublicIp" --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" --output json | jq .[0][0] --raw-output)
-aws ssm start-session \
-	--target $AgentId
+chmod 400 $SshKey
+ssh -i $SshKey ubuntu@$PublicIp
