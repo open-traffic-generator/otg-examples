f6f6e6acffe132b6179d11178ade345b0d6d2d26
armdupre <a-joseph.dupre@keysight.com>
2024-04-23 02:47:24 -0400

    Updated.

--- public-cloud/azure/ixia-c-dpdk-azure-mana/application/deployment/setup.sh
+++ public-cloud/azure/ixia-c-dpdk-azure-mana/application/deployment/setup.sh
@@ -1,16 +1,38 @@
-#!/bin/bash
+# Enable 2MB hugepages.
+echo 2048 | tee /sys/devices/system/node/node*/hugepages/hugepages-2048kB/nr_hugepages
 
-NO_HUGEPAGES=4536
+# Assuming use of eth1 for DPDK in this demo
+PRIMARY="eth1"
 
-mkdir -p /mnt/huge
-mount -t hugetlbfs nodev /mnt/huge
-echo $NO_HUGEPAGES > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
-NR_HUGEPAGES=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages)
+SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
 
-if [ $NR_HUGEPAGES -eq $NO_HUGEPAGES ]
-then
-    echo "$NR_HUGEPAGES 2MB hugepages were allocated."
-    echo "OK"
-else
-    echo "$NR_HUGEPAGES 2MB hugepages were allocated."
-fi
+# Set MANA interfaces DOWN before starting DPDK
+ip link set $PRIMARY down
+ip link set $SECONDARY down
+
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+NET_UUID="f8615163-df3e-46c5-913f-f2d2f965ed0e"
+modprobe uio_hv_generic
+echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
+echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
+
+####################################################################################
+
+# Assuming use of eth2 for DPDK in this demo
+PRIMARY="eth2"
+
+SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
+
+# Set MANA interfaces DOWN before starting DPDK
+ip link set $PRIMARY down
+ip link set $SECONDARY down
+
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+# NET_UUID="f7615163-df3e-46c5-913f-f2d2f965ed0e"
+modprobe uio_hv_generic
+# echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
+echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
