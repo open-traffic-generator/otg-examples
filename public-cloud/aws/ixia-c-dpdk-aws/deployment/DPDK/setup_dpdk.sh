1c4fcbbd419b259cd82af92ca16dfba39fb4c6b2
armdupre <a-joseph.dupre@keysight.com>
2024-04-23 18:00:01 -0400

    Updated.

--- public-cloud/aws/ixia-c-dpdk-aws/deployment/DPDK/setup_dpdk.sh
+++ public-cloud/aws/ixia-c-dpdk-aws/deployment/DPDK/setup_dpdk.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 UserName="ubuntu"
-GitRepoName="keng-python"
-GitRepoBasePath="/home/$UserName/$GitRepoName/public-cloud/aws/ixia-c-dpdk-aws"
-GitRepoExecPath="$GitRepoBasePath/application"
+GitRepoBasePath="/home/$UserName/keng-python"
 GitRepoDeployPath="$GitRepoExecPath/deployment"
+GitRepoExecPath="$GitRepoBasePath/snappi/data-plane-performance"
 AgentEth1LogicalName=$(lshw -C network -json | jq .[1].logicalname --raw-output)
 AgentEth1BusInfo=$(lshw -C network -json | jq .[1].businfo --raw-output)
 echo "AgentEth1BusInfo=$AgentEth1BusInfo" > $GitRepoDeployPath/.env
