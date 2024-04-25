4b0a2e291576afb8a5cd77d2bb20e01dee680eb9
armdupre <a-joseph.dupre@keysight.com>
2024-04-25 15:33:11 -0400

    Updated.

--- public-cloud/azure/ixia-c-dpdk-azure-mana/deployment/Terraform/data.tf
+++ public-cloud/azure/ixia-c-dpdk-azure-mana/deployment/Terraform/data.tf
@@ -20,9 +20,19 @@ data "cloudinit_config" "init_cli" {
 		content = templatefile("cloud-init.azure.yml", {
 			Agent1Eth1PrivateIpAddresses: local.Agent1Eth1IpAddresses
 			Agent1Eth2PrivateIpAddresses: local.Agent1Eth2IpAddresses
+			Agent1Eth3PrivateIpAddresses: local.Agent1Eth3IpAddresses
+			Agent1Eth4PrivateIpAddresses: local.Agent1Eth4IpAddresses
+			Agent1Eth5PrivateIpAddresses: local.Agent1Eth5IpAddresses
+			Agent1Eth6PrivateIpAddresses: local.Agent1Eth6IpAddresses
+			Agent1Eth7PrivateIpAddresses: local.Agent1Eth7IpAddresses
 			Agent2Eth0IpAddress: local.Agent2Eth0IpAddress
 			Agent2Eth1PrivateIpAddresses: local.Agent2Eth1IpAddresses
 			Agent2Eth2PrivateIpAddresses: local.Agent2Eth2IpAddresses
+			Agent2Eth3PrivateIpAddresses: local.Agent2Eth3IpAddresses
+			Agent2Eth4PrivateIpAddresses: local.Agent2Eth4IpAddresses
+			Agent2Eth5PrivateIpAddresses: local.Agent2Eth5IpAddresses
+			Agent2Eth6PrivateIpAddresses: local.Agent2Eth6IpAddresses
+			Agent2Eth7PrivateIpAddresses: local.Agent2Eth7IpAddresses
 			GitRepoConfigPath: local.GitRepoConfigPath
 			GitRepoDeployPath: local.GitRepoDeployPath
 			GitRepoExecDeployPath: local.GitRepoExecDeployPath
