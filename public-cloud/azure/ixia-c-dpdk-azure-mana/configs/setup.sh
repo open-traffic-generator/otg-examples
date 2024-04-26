51b4cd182ae76b6b93bbeda184001c790ea2d534
armdupre <a-joseph.dupre@keysight.com>
2024-04-26 07:37:14 -0400

    Initial Checkin.

---
+++ public-cloud/azure/ixia-c-dpdk-azure-mana/configs/setup.sh
@@ -0,0 +1,133 @@
+# Enable 2MB hugepages.
+echo 2048 | tee /sys/devices/system/node/node*/hugepages/hugepages-2048kB/nr_hugepages
+
+# Assuming use of eth1 for DPDK in this demo
+PRIMARY="eth1"
+
+SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
+
+# Set MANA interfaces DOWN before starting DPDK
+ip link set $PRIMARY down
+ip link set $SECONDARY down
+
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+NET_UUID="f8615163-df3e-46c5-913f-f2d2f965ed0e"
+modprobe uio_hv_generic
+echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
+echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
+
+####################################################################################
+
+# Assuming use of eth2 for DPDK in this demo
+PRIMARY="eth2"
+
+SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
+
+# Set MANA interfaces DOWN before starting DPDK
+ip link set $PRIMARY down
+ip link set $SECONDARY down
+
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+# NET_UUID="f7615163-df3e-46c5-913f-f2d2f965ed0e"
+modprobe uio_hv_generic
+# echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
+echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
+
+####################################################################################
+
+# Assuming use of eth3 for DPDK in this demo
+PRIMARY="eth3"
+
+SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
+
+# Set MANA interfaces DOWN before starting DPDK
+ip link set $PRIMARY down
+ip link set $SECONDARY down
+
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+# NET_UUID="f7615163-df3e-46c5-913f-f2d2f965ed0e"
+modprobe uio_hv_generic
+# echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
+echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
+
+####################################################################################
+
+# Assuming use of eth4 for DPDK in this demo
+PRIMARY="eth4"
+
+SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
+
+# Set MANA interfaces DOWN before starting DPDK
+ip link set $PRIMARY down
+ip link set $SECONDARY down
+
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+# NET_UUID="f7615163-df3e-46c5-913f-f2d2f965ed0e"
+modprobe uio_hv_generic
+# echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
+echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
+
+####################################################################################
+
+# Assuming use of eth5 for DPDK in this demo
+PRIMARY="eth5"
+
+SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
+
+# Set MANA interfaces DOWN before starting DPDK
+ip link set $PRIMARY down
+ip link set $SECONDARY down
+
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+# NET_UUID="f7615163-df3e-46c5-913f-f2d2f965ed0e"
+modprobe uio_hv_generic
+# echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
+echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
+
+####################################################################################
+
+# Assuming use of eth6 for DPDK in this demo
+PRIMARY="eth6"
+
+SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
+
+# Set MANA interfaces DOWN before starting DPDK
+ip link set $PRIMARY down
+ip link set $SECONDARY down
+
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+# NET_UUID="f7615163-df3e-46c5-913f-f2d2f965ed0e"
+modprobe uio_hv_generic
+# echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
+echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
+
+####################################################################################
+
+# Assuming use of eth7 for DPDK in this demo
+PRIMARY="eth7"
+
+SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
+
+# Set MANA interfaces DOWN before starting DPDK
+ip link set $PRIMARY down
+ip link set $SECONDARY down
+
+## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
+DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
+# NET_UUID="f7615163-df3e-46c5-913f-f2d2f965ed0e"
+modprobe uio_hv_generic
+# echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
+echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
