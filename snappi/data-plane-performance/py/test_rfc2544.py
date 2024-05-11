d929daf2b05fbcfbc45624c2a33e4c100755f7da
Daniel-Florin Dosaru <daniel.dosaru@keysight.com>
2024-05-11 12:12:43 +0300

    Update test_rfc2544.py

--- snappi/data-plane-performance/py/test_rfc2544.py
+++ snappi/data-plane-performance/py/test_rfc2544.py
@@ -68,6 +68,7 @@ def test_throughput_rfc2544_n_flows(api, direction, frame_sizes):
             # change direction
             flow.tx_rx.port.rx_names[0], flow.tx_rx.port.tx_name = flow.tx_rx.port.tx_name, flow.tx_rx.port.rx_names[0]
             flow.packet[0].dst.value, flow.packet[0].src.value = flow.packet[0].src.value, flow.packet[0].dst.value 
+            flow.packet[1].dst.value, flow.packet[1].src.value = flow.packet[1].src.value, flow.packet[1].dst.value
         
 
     for size in packet_sizes:
