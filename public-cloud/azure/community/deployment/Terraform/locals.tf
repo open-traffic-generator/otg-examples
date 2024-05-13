06a284e875feb977fd8c871c9896b34178b8fd27
Daniel-Florin Dosaru <daniel.dosaru@keysight.com>
2024-05-14 00:29:01 +0300

    Use Experimental_Boost192

--- public-cloud/azure/community/deployment/Terraform/locals.tf
+++ public-cloud/azure/community/deployment/Terraform/locals.tf
@@ -1,6 +1,6 @@
 locals {
 	AgentUserName = "ubuntu"
-	AgentVmSize = "Experimental_Boost32"
+	AgentVmSize = "Experimental_Boost192"
 	Agent1InstanceId = "agent1"
 	Agent1Eth1IpAddresses = [ "10.0.2.11" ]
 	Agent2Eth0IpAddress = "10.0.10.12"
